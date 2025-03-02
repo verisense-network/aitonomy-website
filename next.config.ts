a9558c2e9546714b8d254b18f0bf7eb753842ca1
rea <hi@rea.ink>
2025-03-02 15:08:20 +0800

    fix: next allow server actions

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     useWasmBinary: true,
+    serverActions: {
+      allowedOrigins: ["aitonomy-website.vercel.app"],
+    },
   },
   transpilePackages: ["jayson"],
 };
