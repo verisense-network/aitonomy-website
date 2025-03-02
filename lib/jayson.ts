6c9d4cda6653a52c51b91ad903d69d77cd27358f
rea <hi@rea.ink>
2025-03-02 14:02:35 +0800

    fix: jayson url

--- lib/jayson.ts
+++ lib/jayson.ts
@@ -1,8 +1,17 @@
 import Jayson from "jayson";
 
-const client = Jayson.client.http({
-  host: process.env.NEXT_PUBLIC_AITONOMY_RPC_HOST,
-  port: Number(process.env.NEXT_PUBLIC_AITONOMY_RPC_PORT),
-});
+let client: Jayson.Client;
+
+const isProduction = process.env.NODE_ENV === "production";
+if (isProduction) {
+  client = Jayson.client.websocket({
+    url: `${process.env.NEXT_PUBLIC_AITONOMY_RPC_HOST}:${process.env.NEXT_PUBLIC_AITONOMY_RPC_PORT}`,
+  });
+} else {
+  client = Jayson.client.http({
+    host: process.env.NEXT_PUBLIC_AITONOMY_RPC_HOST,
+    port: Number(process.env.NEXT_PUBLIC_AITONOMY_RPC_PORT),
+  });
+}
 
 export default client;
