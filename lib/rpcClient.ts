54e8c185067b1629958adc50056f7d419045c93d
rea <hi@rea.ink>
2025-03-02 19:00:33 +0800

    fix:  rpc httpClient unused on

--- lib/rpcClient.ts
+++ lib/rpcClient.ts
@@ -4,14 +4,6 @@ const provider = new HttpProvider(
   `${process.env.NEXT_PUBLIC_AITONOMY_RPC_HOST}:${process.env.NEXT_PUBLIC_AITONOMY_RPC_PORT}`
 );
 
-provider.on("connected", () => {
-  console.log("provider connected");
-});
-
-provider.on("error", (error: any) => {
-  console.error("provider error", error);
-});
-
 export async function getRpcClient() {
   if (!provider.isConnected) {
     await provider.connect();
