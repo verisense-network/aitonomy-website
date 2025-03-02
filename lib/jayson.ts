e369a088e764207d408465030794f03528550e0b
rea <hi@rea.ink>
2025-03-02 14:41:44 +0800

    fix: jayson error log

--- lib/jayson.ts
+++ lib/jayson.ts
@@ -1,17 +1,29 @@
 import Jayson from "jayson";
 
-let client: Jayson.Client;
+let client: Jayson.WebsocketClient | Jayson.HttpClient;
 
 const isProduction = process.env.NODE_ENV === "production";
 if (isProduction) {
   client = Jayson.client.websocket({
     url: `${process.env.NEXT_PUBLIC_AITONOMY_RPC_HOST}:${process.env.NEXT_PUBLIC_AITONOMY_RPC_PORT}`,
   });
+  const wsClient = client as any;
+  (wsClient.ws as any).on("open", () => {
+    console.log("Jayson Connection opened");
+  });
+  (wsClient.ws as any).on("close", () => {
+    console.log("Jayson Connection closed");
+  });
 } else {
   client = Jayson.client.http({
     host: process.env.NEXT_PUBLIC_AITONOMY_RPC_HOST,
     port: Number(process.env.NEXT_PUBLIC_AITONOMY_RPC_PORT),
   });
+  client.on("open", () => {
+    console.log("Jayson Connection opened");
+  });
 }
+
+console.log("client", (client as any).options);
 
 export default client;
