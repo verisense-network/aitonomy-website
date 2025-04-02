cd00dece28530d5565ec1f7b6d4c292cc2873926
rea <hi@rea.ink>
2025-04-02 18:58:51 +0800

    feat: add create community tour

--- components/tour/LearnCard.tsx
+++ components/tour/LearnCard.tsx
@@ -2,25 +2,39 @@ import { Button, Card, CardBody, CardFooter, CardHeader } from "@heroui/react";
 import { StepType, useTour } from "@reactour/tour";
 import { useEffect } from "react";
 
-export type LearnCardProps = {
+type LearnCardProps = {
   title: string;
   content: React.ReactNode;
   steps: StepType[];
+  closeLearnModal: () => void;
 };
 
-export default function LearnCard({ title, content, steps }: LearnCardProps) {
-  const { setSteps, setIsOpen } = useTour();
+export type LearnStep = Omit<LearnCardProps, "closeLearnModal">;
+
+export default function LearnCard({
+  title,
+  content,
+  steps,
+  closeLearnModal,
+}: LearnCardProps) {
+  const { setSteps, setCurrentStep, setIsOpen } = useTour();
 
   useEffect(() => {
     setSteps?.(steps);
   }, [steps, setSteps]);
+
+  const handleStart = () => {
+    setIsOpen(true);
+    setCurrentStep(0);
+    closeLearnModal();
+  };
 
   return (
     <Card>
       <CardHeader>{title}</CardHeader>
       <CardBody>{content}</CardBody>
       <CardFooter>
-        <Button onPress={() => setIsOpen(true)}>Start</Button>
+        <Button onPress={handleStart}>Start</Button>
       </CardFooter>
     </Card>
   );
--- components/tour/tours/createCommunityTour.tsx
+++ components/tour/tours/createCommunityTour.tsx
@@ -1,19 +1,21 @@
-import { LearnCardProps } from "../LearnCard";
+import { useModalStore } from "@/stores/modal";
+import { LearnStep } from "../LearnCard";
 
-export const createCommunityTour: LearnCardProps = {
+export const createCommunityTour: LearnStep = {
   title: "Create a community",
   content: <>learn how to create a community</>,
   steps: [
     {
       selector: ".create-community-step1",
-      content:
-        "This step is attached to the bottom of the <code>.create-community-step1</code> element.",
-      action(elem) {},
+      content: "Click + and select Create Community",
+      actionAfter() {
+        useModalStore.getState().setIsShowCreateCommunity(true);
+      },
     },
     {
       selector: ".create-community-step2",
-      content:
-        "This step is attached to the bottom of the <code>.create-community-step2</code> element.",
+      position: "center",
+      content: "Enter form details and click Create Community",
     },
   ],
 };
