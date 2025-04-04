a6ed56924fcd479110ea9f90529514b73b50f95a
rea <hi@rea.ink>
2025-04-04 21:02:09 +0800

    feat: use google storage

--- lib/uploadImage.ts
+++ lib/uploadImage.ts
@@ -1,17 +1,26 @@
-import { ImageUploadService } from "postimages-upload";
 import { Buffer } from "buffer";
+import storage from "./googleStorage";
 
-const service = new (ImageUploadService as any)(
-  "postimages.org",
-  "8c0744aa3c57541cae0032196ab1ce28"
-);
+const bucketName = "aitonomy-image";
+const destination = "upload-image";
 
 export default async function uploadImageWithPostImages(file: File) {
   const arrayBuffer = await file.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
-  const res = await service.uploadFromBinary(buffer, file.name);
-  if (!res?.directLink) {
+
+  try {
+    const bucket = storage.bucket(bucketName);
+    const bucketFile = bucket.file(`${destination}/${file.name}`);
+
+    await bucketFile.save(buffer);
+
+    const signedUrl = await bucketFile.getSignedUrl({
+      action: "read",
+      expires: Date.now() + 3600 * 1000,
+    });
+    return signedUrl;
+  } catch (error) {
+    console.error("error uploading image", error);
     throw new Error("failed to upload image");
   }
-  return res.directLink;
 }
