17f4d9142bb13de5f9c1a6cc0f280a02bb97d86f
rea <hi@rea.ink>
2025-04-16 15:03:53 +0800

    fix: clean ****

--- components/markdown/utils.ts
+++ components/markdown/utils.ts
@@ -3,5 +3,6 @@ export const cleanContent = (content: string) => {
     .replace(/&#x20;/g, "")
     .replace(/&nbsp;/g, "")
     .replace(/<u>|<\/u>/g, "")
+    .replace(/\*\*\*\*/g, "")
     .trim();
 };
